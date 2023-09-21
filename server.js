ad52c1ca0dd1af52ac591a12da7833b7d80d211f
Jeffry Allan <108166134+jeffry2603@users.noreply.github.com>
2023-09-21 20:26:19 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import  path  from "path";
+import { fileURLToPath } from 'url';
 
 // configure env
 dotenv.config();
+
+//esmodule fix
+const__filename = fileURLToPath(import.meta.url);
+const__dirname = path.dirname(__filename);
 
 //databse config
 connectDB();
